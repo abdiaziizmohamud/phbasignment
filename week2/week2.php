2107d85bbe49e53a1afafea6a7e1dd7c86347420
abdiaziizmohamud <xuudicaamir9@gmail.com>
2026-09-22 10:27:38 +0930

    updated week2

--- week2/week2.php
+++ week2/week2.php
@@ -10,111 +10,117 @@
 
 
 
-    define("Age",123);
-    echo Age;
+//     define("Age",123);
+//     echo Age;
 
 
-    // exmple
-    $Age =20;
+//     // exmple
+//     $Age =20;
 
-    if($Age >=18)
-        echo"Adult";
-    else
-        echo"child";
+//     if($Age >=18)
+//         echo"Adult";
+//     else
+//         echo"child";
 
-    // exmple NP
-
-
-$marks = 100;
-
-switch (true) {
-    case ($marks >= 90):
-        echo "A";
-        break;
-
-    case ($marks >= 80):
-        echo "B";
-        break;
-
-    case ($marks >= 70):
-        echo "C";
-        break;
-
-    case ($marks >= 60):
-        echo "D";
-        break;
-
-    default:
-        echo "F";
-}
+//     // exmple NP
 
 
+// $marks = 100;
 
+// switch (true) {
+//     case ($marks >= 90):
+//         echo "A";
+//         break;
 
-// exmple NP
+//     case ($marks >= 80):
+//         echo "B";
+//         break;
 
+//     case ($marks >= 70):
+//         echo "C";
+//         break;
 
-$fuel = 1;
+//     case ($marks >= 60):
+//         echo "D";
+//         break;
 
-if ($fuel <= 1) {
-    echo "Low";
-} else {
-    echo "Full";
-}
-
-
-// exmple
+//     default:
+//         echo "F";
+// }
 
 
 
 
-$count = 1;
-
-while ($count <= 5) {
-    echo $count;
-    $count++;
-}
+// // exmple NP
 
 
+// $fuel = 1;
+
+// if ($fuel <= 1) {
+//     echo "Low";
+// } else {
+//     echo "Full";
+// }
 
 
-
-
-// exmple
-
-
-$count = 1;
-
-do {
-    echo $count . "<br>";
-    $count++;
-} while ($count <= 5);
+// // exmple
 
 
 
 
-// exmple
+// $count = 1;
+
+// while ($count <= 5) {
+//     echo $count;
+//     $count++;
+// }
 
 
-for ($row = 1; $row <= 5; $row++) {
-    for ($col = 1; $col <= 5; $col++) {
-        $result = $row * $col;
-        echo "$row * $col = $result<br>";
-    }
-}
-
-echo "<br><br>";
-
-// Qaybta labaad ee muujinaysa "Row is X, Column is Y, Result is Z"
-for ($row = 1; $row <= 5; $row++) {
-    for ($col = 1; $col <= 5; $col++) {
-        $result = $row * $col;
-        echo "Row is $row, Column is $col, Result is $result<br>";
-    }
-}
 
 
-echo "<br><br>";
+
+
+// // exmple
+
+
+// $count = 1;
+
+// do {
+//     echo $count . "<br>";
+//     $count++;
+// } while ($count <= 5);
+
+
+
+
+// // exmple
+
+
+// for ($row = 1; $row <= 5; $row++) {
+//     for ($col = 1; $col <= 5; $col++) {
+//         $result = $row * $col;
+//         echo "$row * $col = $result<br>";
+//     }
+// }
+
+// echo "<br><br>";
+
+// // Qaybta labaad ee muujinaysa "Row is X, Column is Y, Result is Z"
+// for ($row = 1; $row <= 5; $row++) {
+//     for ($col = 1; $col <= 5; $col++) {
+//         $result = $row * $col;
+//         echo "Row is $row, Column is $col, Result is $result<br>";
+//     }
+// }
+
+
+
+
+
+
+
+
+
 
     ?>
     
